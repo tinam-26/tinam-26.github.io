4ec6e3154aa9185b6440fadff76f1c8d3d5aa5ac
Tina <18502544+tinam-26@users.noreply.github.com>
2020-01-13 18:30:47 -0500

    updates

--- scripts/test.js
+++ scripts/test.js
@@ -1,2 +1,16 @@
-var myHeading = document.querySelector('h1');
+/* var myHeading = document.querySelector('h1');
+//sdocument.getElementById("demo").innerHTML = "Hello JavaScript!"
 myHeading.textContent = 'Hello world!';
+
+document.querySelector('h1').onclick = function () {
+    alert('text clicked');
+} */
+
+var canvas = document.getElementById("testGameCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20,30,50,60);
+ctx.fillStyle = "#FF0090";
+ctx.fill();
+ctx.closePath();
