84170a30376d3ab1cfb32a3a26ecb461ec97ced0
tinam-26 <cmodica@uncc.edu>
2020-02-05 18:30:40 -0500

    uhhh

--- scripts/test.js
+++ scripts/test.js
@@ -1,16 +1,2 @@
-/* var myHeading = document.querySelector('h1');
-//sdocument.getElementById("demo").innerHTML = "Hello JavaScript!"
+var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-
-document.querySelector('h1').onclick = function () {
-    alert('text clicked');
-} */
-
-var canvas = document.getElementById("testGameCanvas");
-var ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.rect(20,30,50,60);
-ctx.fillStyle = "#FF0090";
-ctx.fill();
-ctx.closePath();
